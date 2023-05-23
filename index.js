40b7d362322d5b9e4d3c362fed5bdfb0d30dfbd3
Tanveer Hossain Jony <thjbd19@gmail.com>
2023-05-23 20:19:42 +0600

    Update index.js

--- index.js
+++ index.js
@@ -26,6 +26,14 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const toyCollection = client.db("A11-toys").collection("toys");
+
+    app.get("/toys", async (req, res) => {
+      const cursor = toyCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -33,7 +41,7 @@ async function run() {
     );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
