f3482e571c4fdb815307577f8f8785eee8520948
Tanveer Hossain Jony <thjbd19@gmail.com>
2023-07-16 23:37:54 +0600

    vercel.json file update
    {
      "version": 2,
      "builds": [
        {
          "src": "./index.js",
          "use": "@vercel/node"
        }
      ],
      "routes": [
        {
          "src": "/(.*)",
          "dest": "index.js",
          "methods": ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"]
        }
      ]
    }

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    client.connect();
+    // client.connect();
 
     const postToyCollection = client.db("A11-toys").collection("postToy");
 
